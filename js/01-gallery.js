482cd514e143d5de510ccb671e4d18c516654a6f
Alona-Liubchenko <aylin888@rambler.ru>
2022-07-29 02:08:01 +0300

    h2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+const cardMarkup = createGallryMarkup(galleryItems);
+container.insertAdjacentHTML("beforeend", cardMarkup);
+container.addEventListener("click", onGalleryClick);
+
+function createGallryMarkup(galleryItems) {
+  return galleryItems
+    .map((el) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+const onGalleryClick = (event) => {
+  event.preventDefault();
+  //   const instance = basicLightbox.create(
+  //     `<img source = ${event.target.dataset.source}/>`
+  //   );
+  //   instance.show();
+};
+
+console.log(createGallryMarkup(galleryItems));
+// console.log(galleryItems);
